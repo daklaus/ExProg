2acddc1f342670d830494a2ddc81ff1afc1dc1ff
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2012-06-04 20:45:54 +0200

    Completed system except for 2 errors

--- test-con.sh
+++ test-con.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-for i in {1..10}; do
-	./test.sh connect connect $i
+FILTEROFF="$1"
+
+FILTER="UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p"
+
+if [ "$FILTEROFF" -eq "1" ]; then
+	FILTER2=",c_max,in_c,out_c,in_p,out_p,c,p"
+fi
+
+for TESTNR in {1..10}; do
+	[ -f connect.test$TESTNR.dl ] || continue
+	dlv -N=60 -silent -pfilter="$FILTER$FILTER2" c.dl psh.dl w.dl f.dl s.dl \
+		connect.dl connect.test$TESTNR.dl connect.tester.dl
 done
--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-TESTER="$1"
-COMPONENT="$2"
-TESTNR="$3"
-FILTERON="$4"
+COMPONENT="$1"
+TESTNR="$2"
+FILTEROFF="$3"
 
 [ -f $COMPONENT.test$TESTNR.dl ] || exit 1
 
-if [ -z "$FILTERON" ] || [ "$FILTERON" -eq "0" ]; then
+if [ -z "$FILTEROFF" ] || [ "$FILTEROFF" -eq "0" ]; then
 	FILTER="UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p"
 fi
 
-dlv -pfilter="$FILTER" $COMPONENT.dl $COMPONENT.test$TESTNR.dl $TESTER.tester.dl -N=60 -silent
+dlv -pfilter="$FILTER" $COMPONENT.dl $COMPONENT.test$TESTNR.dl component.tester.dl -N=60 -silent
