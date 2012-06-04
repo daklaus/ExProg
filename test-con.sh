01109f08e66a1dccb9fc4fbf2cc1c742d9fb5bd2
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2012-06-04 05:12:41 +0200

    Added other components and connections (all but con-tests)

--- test-con.sh
+++ test-con.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {1..10}; do
-	./test.sh component "$1" $i
+	./test.sh connect connect $i
 done
--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-COMPONENT="$1"
-TESTNR="$2"
+TESTER="$1"
+COMPONENT="$2"
+TESTNR="$3"
+FILTERON="$4"
 
-if [ -n "$3" ] && [ "$3" -eq "1" ]; then
+[ -f $COMPONENT.test$TESTNR.dl ] || exit 1
+
+if [ -z "$FILTERON" ] || [ "$FILTERON" -eq "0" ]; then
 	FILTER="UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p"
 fi
 
-dlv -pfilter="$FILTER" $COMPONENT.dl $COMPONENT.test$TESTNR.dl component.tester.dl -N=60 -silent
+dlv -pfilter="$FILTER" $COMPONENT.dl $COMPONENT.test$TESTNR.dl $TESTER.tester.dl -N=60 -silent
